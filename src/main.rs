0555b8180f51fa91097ab1e533fadb9c69cf6d61
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-07-08 21:15:53 -0500

    Rustfmt fix
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use notify::{Watcher, RecursiveMode, watcher};
-use std::sync::mpsc::channel;
-use clap::Parser;
 use anyhow::Error;
+use clap::Parser;
+use notify::{watcher, RecursiveMode, Watcher};
 use std::process::Command;
+use std::sync::mpsc::channel;
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -13,7 +13,6 @@ use std::process::Command;
     long_about = None
 )]
 pub struct Args {
-
     /// The debounce period (i.e. wait for a duration of X before running the specified executable).
     #[clap(short = 't', long, value_parser, default_value = "600ms")]
     pub debounce_period: String,
@@ -28,39 +27,35 @@ pub struct Args {
 
     /// An executable to run once a change is detected.
     #[clap(short = 'c', long, value_parser)]
-    pub command_file: String
+    pub command_file: String,
 }
 
 /// The main function.
-fn main() -> Result<(), Error>{
-    
+fn main() -> Result<(), Error> {
     let args = Args::parse();
     let duration = parse_duration::parse(&args.debounce_period)?;
 
     let (tx, rx) = channel();
     let mut watcher = watcher(tx, duration)?;
-    
+
     watcher.watch(
         args.file.clone(),
         match args.recursive {
             true => RecursiveMode::Recursive,
-            false => RecursiveMode::NonRecursive
-        }
+            false => RecursiveMode::NonRecursive,
+        },
     )?;
 
     loop {
         match rx.recv() {
             Ok(_) => {
-                if let Err(e) = Command::new(args.command_file.clone())
-                    .spawn()
-                {
+                if let Err(e) = Command::new(args.command_file.clone()).spawn() {
                     eprintln!(
                         "Error occurred when command {:?} was executed: {:?}",
-                        e,
-                        &args.command_file
+                        e, &args.command_file
                     );
                 }
-            },
+            }
             Err(e) => {
                 eprintln!("Failed to watch {e:?}");
             }
